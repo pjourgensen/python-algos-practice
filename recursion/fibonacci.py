b01a50ed8a37f687a29d1c7fe161f3845b88c31f
Peter Jourgensen <48814862+pjourgensen@users.noreply.github.com>
2019-07-11 18:15:13 -0700

    Create fibonacci.py

---
+++ recursion/fibonacci.py
@@ -0,0 +1,12 @@
+"""
+Returns n-th fibonacci number using recursion.
+"""
+
+def fibonacci(n):
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
